f7cbd6dfcea92f674173c8e267955ae0a473aa32
Kang Sheng Tay <kangsheng@gmail.com>
2017-03-02 14:18:08 +0800

    MVC for feature/comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'faq/index'
 
+  resources :comment
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
